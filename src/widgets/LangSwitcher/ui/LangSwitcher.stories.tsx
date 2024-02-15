0b2ea4cfb5686b99688cf267c5e29a28f574faae
chrisscappp <chrisscappp@mail.ru>
2024-02-15 11:51:22 +0300

    add loki, add stories test for langswitcher

---
+++ src/widgets/LangSwitcher/ui/LangSwitcher.stories.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+import type { Meta, StoryObj } from '@storybook/react';
+import { LangSwitcher} from './LangSwitcher';
+import { ThemeDecorator } from "shared/config/storybookConfig/themeDecorator/themeDecorator";
+import { Themes } from "app/providers/ThemeProvider/index"
+
+const meta: Meta<typeof LangSwitcher> = {
+	title: "widget/LangSwitcher",
+  	component: LangSwitcher,
+};
+
+export default meta;
+type Story = StoryObj<typeof LangSwitcher>;
+
+export const LangSwitcherLight: Story = {
+
+};
+
+export const LangSwitcherDark: Story = {
+	decorators: [ThemeDecorator(Themes.DARK)]
+};
+
