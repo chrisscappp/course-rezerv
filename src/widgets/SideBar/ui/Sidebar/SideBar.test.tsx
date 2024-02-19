6ae7974e3bf0e4aac3ec59097228140c54bc5c05
chrisscappp <chrisscappp@mail.ru>
2024-02-19 21:52:15 +0300

    fix tests

--- src/widgets/SideBar/ui/Sidebar/SideBar.test.tsx
+++ src/widgets/SideBar/ui/Sidebar/SideBar.test.tsx
@@ -1,15 +1,16 @@
 /* eslint-disable i18next/no-literal-string */
 import { Sidebar } from "widgets/SideBar/index"
-import { fireEvent, render, screen } from "@testing-library/react"
+import { fireEvent, screen } from "@testing-library/react"
+import { componentRender } from "shared/lib/tests/componentRender/componentRender"
 
 describe('Sidebar', () => {
 	test('test sidebar', () => {
-		render(<Sidebar/>)
+		componentRender(<Sidebar/>)
 		expect(screen.getByTestId("sidebar")).toBeInTheDocument()
 	})
 
 	test('test sidebar toggle', () => {
-		render(<Sidebar/>)
+		componentRender(<Sidebar/>)
 		const toggleBtn = screen.getByTestId("sidebar-toggle")
 		expect(screen.getByTestId("sidebar")).toBeInTheDocument()
 		expect(screen.getByTestId("sidebar")).toHaveClass('collapsed')
