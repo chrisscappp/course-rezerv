12c0c22020a3d40272332d1c66356985cf32012c
chrisscappp <chrisscappp@mail.ru>
2024-02-27 22:02:38 +0300

    fix pre-commit

--- src/feautures/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/feautures/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,5 +1,4 @@
 import { classNames } from "shared/lib/classNames/classNames"
-import cls from "./LoginModal.module.scss"
 import React from "react";
 import { Modal } from "shared/ui/Modal/Modal";
 import { LoginForm } from "../LoginForm/LoginForm";
@@ -14,7 +13,7 @@ export const LoginModal = ({ className, isOpen, onClose }: LoginModalProps) => {
 
 	return (
 		<Modal 
-			className = {classNames(cls.LoginModal, {}, [className])}
+			className = {classNames('', {}, [className])}
 			isOpen = {isOpen}
 			onClose = {onClose}
 			lazy
